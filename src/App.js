1ecf7c358bc733963747012c26c2572d741e6135
Arthur Picard <arthur.picard7@gmail.com>
2022-10-18 16:08:04 +0200

    Add created Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 import './App.scss';
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Router> */}
+        <div className="App">
+          <Navbar />
+          <Banner />
+          <Projects  />
+          <About />
+          {/* <Routes> */}
+            {/* <Route path='/' element={<Banner />} /> */}
+            {/* <Route path='/projects' element={<Projects />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<ErrorPage />} /> */}
+          {/* </Routes> */}
+          {/* <Footer /> */}
+        </div>
+      {/* </Router> */}
     </>
   );
 }
